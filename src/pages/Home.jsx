3601019bfbd0692b60c59146b3df1026a48e39e9
Alberto Pérez <apleon89@gmail.com>
2023-03-14 20:00:42 +0000

    css a la página home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,38 @@
 import { Link } from "react-router-dom";
+import "./Home.css";
+import video from "../assets/video/IMG_0021.MP4";
 
 function Home() {
   return (
-    <>
-      <h2>Re-Usa</h2>
-      <Link to="/registro">
-        <button>Regístrate</button>
-      </Link>
-      <Link to="/acceso">
-        <button>Entrar</button>
-      </Link>
-      <Link to="/anuncios">
-        <button>Ver anuncios</button>
-      </Link>
-    </>
+    <div className="body">
+      {/* <h2 className="titulo">Re-Usa</h2> */}
+      <video autoPlay muted loop className="video-home">
+        <source src={video} type="video/mp4" />
+      </video>
+      <div>
+        <p className="eslogan-text">
+          ¡Reduce, reutiliza, Re-Usa! Tu app para intercambiar productos de
+          segunda mano.
+        </p>
+        <div>
+          <p className="frase-anuncios">Echa un vistazo a los anuncios:</p>
+          <Link to="/anuncios">
+            <button className="btn btn-anuncios">Ver anuncios</button>
+          </Link>
+        </div>
+        <div>
+          <Link to="/registro">
+            <button className="btn btn-registro">Regístrate</button>
+          </Link>
+          <Link to="/acceso">
+            <button className="btn btn-acceso">Entrar</button>
+          </Link>
+        </div>
+        <footer>
+          <p className="footer">Un proyecto de: Alberto Pérez León.</p>
+        </footer>
+      </div>
+    </div>
   );
 }
 
