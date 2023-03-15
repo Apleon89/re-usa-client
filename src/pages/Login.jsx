bb20402c7f0611fa877606a29ce6660359181a64
Alberto Pérez <apleon89@gmail.com>
2023-03-15 10:57:07 +0000

    estilo a la página de acceso

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,12 +3,13 @@ import { Link, useNavigate } from "react-router-dom";
 import { loginService } from "../services/auth.services";
 import { useContext } from "react";
 import { authContext } from "../context/auth.context";
+import "./Login.css";
 
 function Login() {
   const navigate = useNavigate();
-  
+
   const { validateToken } = useContext(authContext);
-  
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
@@ -34,40 +35,43 @@ function Login() {
     }
   };
   return (
-    <>
-      <h2>Inicia sesión</h2>
-      <form>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="text"
-            id="email"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Contraseña:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div>{errorMessage !== "" && <h2>{errorMessage}</h2>}</div>
-        <button onClick={handleLogin}>Entrar</button>
-      </form>
-      <p>¿No tienes cuenta? Regístrate</p>
-      <Link to="/registro">
-        <button>Regístrase</button>
-      </Link>
+    <div className="body-login">
+      <h2 className="entra-titulo">Inicia sesión</h2>
+      <div>
+        <form className="login-form">
+          <div className="login-div">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="login-div">
+            <label htmlFor="password">Contraseña:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>{errorMessage !== "" && <h2>{errorMessage}</h2>}</div>
+          <button className="btn" onClick={handleLogin}>
+            Entrar
+          </button>
+        </form>
+      </div>
+      <p className="link-acceso-login">
+        ¿No tienes cuenta? <Link to="/registro">Regístrate</Link>
+      </p>
       <Link to="/anuncios">
-        <button>Ver anuncios</button>
+        <button className="btn">Ver anuncios</button>
       </Link>
-    </>
+    </div>
   );
 }
 
