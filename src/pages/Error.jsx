439f4d28bdc37a8cac07d86beeb6cf6d28b72fe9
Alberto Pérez <apleon89@gmail.com>
2023-03-16 13:17:59 +0000

    estilos a la página de error

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import './Error.css'
 
 function Error() {
-    return (
-      <div>
-        <h3>Error 500. Problemas de servidor :(</h3>
-        <Link to='/anuncios'>Anuncios</Link>
-      </div>
-    );
-  }
-  
-  export default Error;
+  return (
+    <div className="error-div">
+      <Navbar />
+      <h3 className="error">Error 500. Problemas de servidor :(</h3>
+      <h4 className="error">Problemas de usar alojamientos gratuitos...</h4>
+      <h4 className="error">Por favor recarga la página o vuelve a intentarlo en unos minutos</h4>
+    </div>
+  );
+}
+
+export default Error;
