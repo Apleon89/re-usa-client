a9a347a1101908792c880e82dbf916b3dc2dd9c4
Alberto Pérez <apleon89@gmail.com>
2023-04-26 19:12:00 +0100

    indicador número foto al carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from "react";
-import './Carousel.css'
+import "./Carousel.css";
 
 function Carousel(props) {
   const { adImages } = props;
 
   const [counter, setCounter] = useState(0);
+  const [totalImg, setTotalImg] = useState(1);
   const [actualImage, setActualImage] = useState(adImages[counter]);
 
   useEffect(() => {
     setActualImage(adImages[counter]);
+    adImages.map((each, i) => {
+      if (
+        each !==
+        "https://res.cloudinary.com/dacltsvln/image/upload/v1678707296/re-Usa/eca7lptfqlqstm28zorb.png"
+      ) {
+        setTotalImg(i + 1);
+      }
+    });
   }, [counter]);
 
   const rightImg = () => {
@@ -30,10 +39,11 @@ function Carousel(props) {
   return (
     <div className="carousel-body">
       <div className="carousel-img-container">
-      <img src={actualImage} alt="anuncio img" />
+        <img src={actualImage} alt="anuncio img" />
       </div>
       <div className="carousel-btn">
         <button onClick={leftImg}> ← </button>
+        <p>{`${counter + 1}/${totalImg}`}</p>
         <button onClick={rightImg}> → </button>
       </div>
     </div>
