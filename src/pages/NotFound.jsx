61905a6789981bbdaa211a2860dd4ae544777703
Alberto Pérez <apleon89@gmail.com>
2023-05-02 10:48:33 +0100

    diseño y estilos página 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,21 @@
+import { Link, useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import "./NotFound.css";
+import errorImg from "../assets/images/Error404-mobile.png";
+
 function NotFound() {
-    return (
-      <div>
-        <h3>Error 404. Pagina no encontrada :(</h3>
-      </div>
-    );
-  }
-  
-  export default NotFound;
-  
+  const navigate = useNavigate();
+
+  return (
+    <div className="nofFound-div">
+      <Navbar />
+      <img className="notFoundImgMobile" src={errorImg} alt="404" />
+      <h3>Error 404. Pagina no encontrada :(</h3>
+      <Link className="go-back-404Btn" to={navigate(-1)}>
+        <button className="btn go-back-404Btn">Vuelve atrás</button>
+      </Link>
+    </div>
+  );
+}
+
+export default NotFound;
