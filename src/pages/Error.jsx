e1db217f397cd7f71ff271dca4ac31bdda6b30ff
Alberto Pérez <apleon89@gmail.com>
2023-05-02 13:01:27 +0100

    diseño y estilos página error 500

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,14 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import './Error.css'
+import "./Error.css";
+import error500 from '../assets/images/error-500.png'
 
 function Error() {
+  const navigate = useNavigate();
+
   return (
     <div className="error-div">
       <Navbar />
+      <img className="error500Img" src={error500} alt="500" />
       <h3 className="error">Error 500. Problemas de servidor :(</h3>
-      <h4 className="error">Problemas de usar alojamientos gratuitos...</h4>
-      <h4 className="error">Por favor recarga la página o vuelve a intentarlo en unos minutos</h4>
+      <h4 className="error">¡ El alojamiento es gratuito..!</h4>
+      <Link to={navigate(-1)}>
+        <button className="btn go-back-500Btn">Inténtalo de nuevo</button>
+      </Link>
     </div>
   );
 }
